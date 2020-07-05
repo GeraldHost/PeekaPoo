fb10ab22b96068b8683c5f957e6c02c58de67dd9
Gerald <me@jacobford.co.uk>
2020-07-05 20:14:24 +0100

    feat(ani): memo pan

--- src/usePanAnimation.js
+++ src/usePanAnimation.js
@@ -16,34 +16,38 @@ export const usePanAnimation = () => {
 
   linearAnimation.addListener(({ value }) => (this._value = value));
 
-  const panResponder = PanResponder.create({
-    onStartShouldSetPanResponder: () => true,
-    onPanResponderMove: (_, { dx }) => {
-      const progress = Math.abs(dx) / w;
-      linearAnimation.setValue(progress * 50);
-    },
-    onPanResponderGrant: () => {
-      const offset =
-        typeof this._value === "undefined" || this._value === 200
-          ? 0
-          : this._value;
-      linearAnimation.setOffset(offset);
-      linearAnimation.setValue(0);
-    },
-    onPanResponderRelease: (_, { dx }) => {
-      // const back = dx < 0;
-      const progress = Math.abs(dx) / w;
-      const reset = progress < 0.4;
-      if (reset) {
-        timing(linearAnimation, 0);
-      } else {
-        timing(linearAnimation, 50, 100, () => {
-          setIndex(index + 1);
-          timing(linearAnimation, 100, 300);
-        });
-      }
-    },
-  });
+  const panResponder = useMemo(
+    () =>
+      PanResponder.create({
+        onStartShouldSetPanResponder: () => true,
+        onPanResponderMove: (_, { dx }) => {
+          const progress = Math.abs(dx) / w;
+          linearAnimation.setValue(progress * 50);
+        },
+        onPanResponderGrant: () => {
+          const offset =
+            typeof this._value === "undefined" || this._value === 200
+              ? 0
+              : this._value;
+          linearAnimation.setOffset(offset);
+          linearAnimation.setValue(0);
+        },
+        onPanResponderRelease: (_, { dx }) => {
+          // const back = dx < 0;
+          const progress = Math.abs(dx) / w;
+          const reset = progress < 0.4;
+          if (reset) {
+            timing(linearAnimation, 0);
+          } else {
+            timing(linearAnimation, 50, 100, () => {
+              setIndex(index + 1);
+              timing(linearAnimation, 100, 300);
+            });
+          }
+        },
+      }),
+    [index, this._value]
+  );
 
   return { panResponder, index, linearAnimation };
 };
