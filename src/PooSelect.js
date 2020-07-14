9042e900149e9175ee5ea5c5eca4a36630b6dfdf
Gerald <me@jacobford.co.uk>
2020-07-14 19:23:37 +0100

    feat(pooSelect): nav

--- src/PooSelect.js
+++ src/PooSelect.js
@@ -1,15 +1,16 @@
 import React from "react";
 
+
 import { Base, Poop, Lower, Button } from "./components";
 import { usePanAnimation } from "./usePanAnimation";
 
-export default function PoopSelect() {
+export default function PoopSelect({ next }) {
   const { linearAnimation, panResponder, index } = usePanAnimation();
   return (
     <Base {...panResponder.panHandlers} linearAnimation={linearAnimation}>
       <Lower linearAnimation={linearAnimation} />
       <Poop linearAnimation={linearAnimation} index={index} />
-      <Button text="Select" />
+      <Button text="Select" onPress={next} />
     </Base>
   );
 }
