96a317ad4a69f00558acb50d7113f6f662becbc5
Gerald <me@jacobford.co.uk>
2020-07-14 19:45:59 +0100

    feat(farts): added fart sounds

--- src/PooSelect.js
+++ src/PooSelect.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Sound from "react-native-sound";
 
+Sound.setCategory("Playback");
+
+const fart = () => {
+  const fartIndex = Math.round(Math.random() * 9);
+  console.log("FI", fartIndex);
+  const sound = new Sound(
+    `farts/${fartIndex}.wav`,
+    Sound.MAIN_BUNDLE,
+    (error) => {
+      if (error) {
+        console.log("fart failed", error);
+        return;
+      }
+      sound.play((success) => {
+        if (success) console.log("farted");
+      });
+    }
+  );
+};
 
 import { Base, Poop, Lower, Button } from "./components";
 import { usePanAnimation } from "./usePanAnimation";
 
 export default function PoopSelect({ next }) {
   const { linearAnimation, panResponder, index } = usePanAnimation();
+
+  useEffect(() => {
+    const timer = setTimeout(() => fart(), 400);
+    return () => clearTimeout(timer);
+  }, [index]);
   return (
     <Base {...panResponder.panHandlers} linearAnimation={linearAnimation}>
       <Lower linearAnimation={linearAnimation} />
